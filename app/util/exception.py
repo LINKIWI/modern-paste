2a3854dc4ef6a569960d75dedee612a109b7afb9
Kevin Lin <developer@kevinlin.info>
2016-01-06 13:48:53 -0800

    Exceptions class

---
+++ app/util/exception.py
@@ -0,0 +1,18 @@
+# User
+
+class UsernameNotAvailableException(Exception):
+    pass
+
+
+class UserDoesNotExistException(Exception):
+    pass
+
+
+class InvalidEmailException(Exception):
+    pass
+
+
+# Crytography
+
+class InvalidIDException(Exception):
+    pass
