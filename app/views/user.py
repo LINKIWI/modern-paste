17a517712ff9c2227de325a77e4d6e8e3ebb035b
Kevin Lin <developer@kevinlin.info>
2016-01-13 11:25:02 -0600

    Logout frontend endpoint

--- app/views/user.py
+++ app/views/user.py
@@ -1,9 +1,21 @@
-from modern_paste import app
-from uri.user import *
+from flask import redirect
+from flask.ext.login import logout_user
+
+from api.decorators import hide_if_logged_in
 from api.decorators import render_view
+from modern_paste import app
+from uri.main import *
+from uri.user import *
 
 
 @app.route(UserLoginInterfaceURI.path, methods=['GET'])
+@hide_if_logged_in(redirect_uri=HomeURI.uri())
 @render_view
 def user_login_interface():
     return 'user/login.html', {}
+
+
+@app.route(UserLogoutInterfaceURI.path, methods=['GET'])
+def user_logout_interface():
+    logout_user()
+    return redirect(HomeURI.uri())
