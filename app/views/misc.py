d761454590714229739d495356389fcc56b1b219
Kevin Lin <developer@kevinlin.info>
2016-01-20 16:03:53 -0600

    Skeleton for misc views test

--- app/views/misc.py
+++ app/views/misc.py
@@ -1,7 +1,3 @@
-from flask import redirect
-from flask.ext.login import logout_user
-
-from api.decorators import hide_if_logged_in
 from api.decorators import render_view
 from modern_paste import app
 from uri.misc import *
---
+++ tests/test_views/test_misc.py
@@ -0,0 +1,8 @@
+import util.cryptography
+import util.testing
+
+
+class TestMisc(util.testing.DatabaseTestCase):
+    def test_api_documentation_interface(self):
+        # TODO
+        pass
