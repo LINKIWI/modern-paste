b2d592862458c2adb585cd8a3722a9f671ecccc2
Kevin Lin <developer@kevinlin.info>
2016-01-21 13:04:59 -0600

    API documentation page, programatically loaded from JSON description

--- app/views/misc.py
+++ app/views/misc.py
@@ -1,3 +1,6 @@
+import os
+import json
+
 from api.decorators import render_view
 from modern_paste import app
 from uri.misc import *
@@ -6,4 +9,10 @@ from uri.misc import *
 @app.route(APIDocumentationInterfaceURI.path, methods=['GET'])
 @render_view
 def api_documentation_interface():
-    return 'misc/api_documentation.html', {}
+    api_endpoints = json.loads(open(os.path.join(
+        os.path.dirname(os.path.abspath(__file__)),
+        '../templates/misc/api_endpoints.json',
+    )).read())['api_endpoints']
+    return 'misc/api_documentation.html', {
+        'api_endpoints': api_endpoints,
+    }
