ae39e9a735cf64df87010206a5a4d757a5f13f5e
Kevin Lin <developer@kevinlin.info>
2016-01-22 12:41:11 -0600

    API listing box on documentation page

--- app/static/js/misc/APIDocumentationController.js
+++ app/static/js/misc/APIDocumentationController.js
@@ -3,9 +3,58 @@ goog.provide('modernPaste.misc.APIDocumentationController');
 goog.require('modernPaste.universal.URIController');
 
 
+/**
+ * This controller handles UI elements on the API documentation page.
+ *
+ * @constructor
+ */
 modernPaste.misc.APIDocumentationController = function() {
+    this.apiListing = $('.api-listing');
+    this.apiDocumentation = $('.api-documentation-container');
+
     hljs.initHighlightingOnLoad();
-}
+
+    $('a').on('click', modernPaste.misc.APIDocumentationController.animateAnchorJump);
+    $(window).scroll(modernPaste.misc.APIDocumentationController.lockApiListing.bind(this));
+    $(window).resize(modernPaste.misc.APIDocumentationController.toggleApiListingVisibility.bind(this));
+};
+
+/**
+ * Toggle the visibility of the API listing on the right side of the page depending on the width of the device's screen.
+ */
+modernPaste.misc.APIDocumentationController.toggleApiListingVisibility = function() {
+    var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    if (width > 1400) {
+        this.apiListing.show();
+    } else {
+        this.apiListing.hide();
+    }
+};
+
+/**
+ * Animate the scrolling when the user clicks a link in the API listing box.
+ */
+modernPaste.misc.APIDocumentationController.animateAnchorJump = function() {
+    $('html, body').animate({
+        'scrollTop': $( $.attr(this, 'href') ).offset().top
+    }, {
+        'duration': 1000,
+        'easing': 'easeOutQuint'
+    });
+};
+
+/**
+ * Stick the API listing to the page if the user has scrolled past the main header.
+ */
+modernPaste.misc.APIDocumentationController.lockApiListing = function() {
+    var distance = $(window).scrollTop();
+
+    if (distance > this.apiDocumentation.offset().top - 140) {
+        this.apiListing.addClass('stick');
+    } else {
+        this.apiListing.removeClass('stick');
+    }
+};
 
 
 $(document).ready(function() {
